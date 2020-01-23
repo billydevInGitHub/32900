9592f4f68f9129d532c4d86204ae8691a4bfcb2a
billy <billydev@gmail.com>
2020-01-23 11:09:59 -0700

    add mediate  pattern and improve visitor pattern

--- src/b32900_Exp021_VisitorPattern/Test.java
+++ src/b32900_Exp021_VisitorPattern/Test.java
@@ -8,7 +8,8 @@ class MyVisitor implements Visitor {
 	  
     @Override  
     public void visit(Subject sub) {  
-        System.out.println("visit the subject："+sub.getSubject());  
+        System.out.println("visit the subject:"+sub.getSubject());  
+        //sub.getSomePrivateInfo(); visitor is not allow to see this private info
     }  
 }  
 
@@ -23,13 +24,20 @@ class MySubject implements Subject {
 	  
     @Override  
     public void accept(Visitor visitor) {  
-        visitor.visit(this);  
+        visitor.visit(this);  //Billy: I think when MySubject is implemented
+                               //like iterate through file tree, it will only give the visitor parameters
+                               //so visitor can only access the resource it is given and that resouce's public method
+                               //I think the implementation is calling back the visitor's methods !!
     }  
   
     @Override  
     public String getSubject() {  
         return "love";  
     }  
+    //the following added by billy
+    private String getSomePrivateInfo() {
+    	return "private information of MySubject";
+    }
 }  
 
 public class Test {
